7f7203559749f4d978c3ba1aeb57858ad729a9f9
nightowl <34909304+night-owl-018@users.noreply.github.com>
2025-12-03 19:08:48 -0800

    Update __init__.py

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,4 +1,9 @@
-# required so Python treats this folder as a package
-app/
-  core/
-    __init__.py   ← REQUIRED
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__, template_folder="web/frontend")
+
+    from .routes import bp
+    app.register_blueprint(bp)
+
+    return app
